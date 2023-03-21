3737ce54d9c12a0e6a1c5305c1920cb28df6c09d
firdous alam <firdous.alam2058@gmail.com>
2023-03-21 15:55:58 +0530

    commit through VS code

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
-// nnnnahhssh
 app.get('/', (req, res) => {
   res.send('Hello World!  To My Technophile Family.')
 })
-// new test 
 app.listen(port, () => {
   console.log(`Hello World App listening on port ${port}`)
 })
